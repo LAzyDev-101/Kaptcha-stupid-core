5784c5c6979c3823d1f974ba5af22a4a6319d1a8
markkj <khajohnyosbk@gmail.com>
2022-07-02 15:31:40 +0700

    add sent next challenge name in response if not finish

--- app/app.go
+++ app/app.go
@@ -1,38 +1,61 @@
 package app
 
 var (
-	users map[string]int
+	users map[string][]string
 )
 
+var allChallenge = []string{
+	"stupid-run",
+	"stupid-block",
+	"stupid-bird",
+	"stupid-memory",
+}
+
 type RequestParams struct {
-	Key string `json:"key"`
+	Key           string `json:"key"`
+	ChallengeName string `json:"challenge_name"`
 }
 
 type RequestResponse struct {
-	Message string `json:"message"`
-	Status  string `json:"status"`
+	Message           string `json:"message"`
+	Status            string `json:"status"`
+	NextChallengeName string `json:"next_challenge_name"`
 }
 
 func ProcessRequest(params RequestParams) (*RequestResponse, error) {
-	count, exist := users[params.Key]
+	challengePlayed, exist := users[params.Key]
 	if !exist {
-		users[params.Key] = 1
-	} else {
-		if count == 3 {
-			return &RequestResponse{
-				Message: "Finish",
-				Status:  "FINISH",
-			}, nil
+		users[params.Key] = make([]string, 0)
+	}
+	if len(challengePlayed) == 4 {
+		return &RequestResponse{
+			Message:           "Finish",
+			Status:            "FINISH",
+			NextChallengeName: "",
+		}, nil
+	}
+	users[params.Key] = append(users[params.Key], params.ChallengeName)
+
+	encountered := make(map[string]bool)
+	for _, playedChallenge := range users[params.Key] {
+		encountered[playedChallenge] = true
+	}
+
+	nextChallengeName := ""
+	for _, challenge := range allChallenge {
+		if _, exist := encountered[challenge]; !exist {
+			nextChallengeName = challenge
+			break
 		}
-		users[params.Key] += 1
 	}
 
 	return &RequestResponse{
-		Message: "next challenge",
-		Status:  "Processing",
+		Message:           "next challenge",
+		Status:            "PROCESSING",
+		NextChallengeName: nextChallengeName,
 	}, nil
 }
 
 func init() {
-	users = make(map[string]int)
+	users = make(map[string][]string)
 }

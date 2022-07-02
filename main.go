2f985317023b30c4c34582baf70ae52a14ccbc24
markkj <khajohnyosbk@gmail.com>
2022-07-02 19:17:34 +0700

    update add cors

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -13,12 +14,30 @@ func serveStatic(path string) http.Handler {
 	return http.StripPrefix("static", http.FileServer(http.Dir(path)))
 }
 
+func accessControlMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+		body, _ := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+
+		log.Printf("%s %s %s", r.Method, r.URL, string(body))
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 
 	app := &app.AppCaptcha{
 		Users: make(map[string][]string),
 	}
 	router := mux.NewRouter()
+	router.Use(accessControlMiddleware)
 
 	// router.Handle(
 	// 	"/memory-game",
@@ -28,14 +47,14 @@ func main() {
 
 	// router.Handle("/memory-game", http.StripPrefix("/static/memory-game", fs))
 	s := http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))
-	router.PathPrefix("/static/").Handler(s)
+	router.PathPrefix("/").Handler(s)
 
 	router.HandleFunc(
 		"/post_finish",
 		func(w http.ResponseWriter, r *http.Request) {
 			api.PostChallenge(app, w, r)
 		},
-	)
+	).Methods("POST")
 
 	log.Print("Listening on :3000...")
 	err := http.ListenAndServe(":3000", router)
